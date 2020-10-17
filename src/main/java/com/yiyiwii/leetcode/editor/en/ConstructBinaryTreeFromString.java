ba54d4b57a623285da7e83b10abd9c6b65b6309c
yiyiwii <donghua.cai@outlook.com>
2020-10-17 09:15:51 -0400

    LC536 Construct binary tree from string

--- src/main/java/com/yiyiwii/leetcode/editor/en/ConstructBinaryTreeFromString.java
+++ src/main/java/com/yiyiwii/leetcode/editor/en/ConstructBinaryTreeFromString.java
@@ -1,14 +1,18 @@
 
 package com.yiyiwii.leetcode.editor.en;
-public class ConstructBinaryTreeFromBinaryTree {
+
+import java.util.Stack;
+
+public class ConstructBinaryTreeFromString {
     public static void main(String[] args) {
-        Solution solution = new ConstructBinaryTreeFromBinaryTree().new Solution();
+        Solution solution = new ConstructBinaryTreeFromString().new Solution();
 //        [1,2,3,4]
-        TreeNode root = new ConstructBinaryTreeFromBinaryTree().new TreeNode(1);
-        root.left = new ConstructBinaryTreeFromBinaryTree().new TreeNode(2);
-        root.right = new ConstructBinaryTreeFromBinaryTree().new TreeNode(3);
-        root.left.left = new ConstructBinaryTreeFromBinaryTree().new TreeNode(4);
-        solution.str2tree(root);
+//        TreeNode root = new ConstructBinaryTreeFromString().new TreeNode(1);
+//        root.left = new ConstructBinaryTreeFromString().new TreeNode(2);
+//        root.right = new ConstructBinaryTreeFromString().new TreeNode(3);
+//        root.left.left = new ConstructBinaryTreeFromString().new TreeNode(4);
+        String str = "-4(2(3)(1))(6(5))";
+        solution.str2tree(str);
     }
     //leetcode submit region begin(Prohibit modification and deletion)
     /**
@@ -28,14 +32,28 @@ public class ConstructBinaryTreeFromBinaryTree {
     }
 
     class Solution {
-        public String str2tree(TreeNode t) {
-            StringBuilder sb = new StringBuilder("");
-            dfs(t, sb);
-            String res = sb.toString();
-            return res.length() > 0 ? res.substring(1) : res;
-        }
-        public void dfs(TreeNode t, StringBuilder sb) {
-
+        public TreeNode str2tree(String s) {
+            if (s == null || s.isEmpty())
+                return null;
+            Stack<TreeNode> stack = new Stack();
+            for (int i = 0; i < s.length(); i++) {
+                char curr = s.charAt(i);
+                if (curr == ')') {
+                    stack.pop();
+                } else if (Character.isDigit(curr) || curr == '-') {
+                    int j = i;
+                    while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1)))
+                        i++;
+                    TreeNode node = new TreeNode(Integer.parseInt(s.substring(j, i + 1)));
+                    if (!stack.isEmpty()) {
+                        TreeNode root = stack.peek();
+                        if (root.left == null) root.left = node;
+                        else root.right = node;
+                    }
+                    stack.push(node);
+                }
+            }
+            return stack.peek();
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
