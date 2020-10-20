4358097274518faf26d513c5db6fc9b779b2ae71
yiyiwii <donghua.cai@outlook.com>
2020-10-20 14:00:21 -0400

    LC235/236 LCA

--- README.md
+++ README.md
@@ -12,8 +12,10 @@
 |606|[Construct String from Binary Tree](https://leetcode.com/problems/construct-string-from-binary-tree/)|[Java](https://github.com/yiyiwii/leetcodeProb/blob/master/src/main/java/com/yiyiwii/leetcode/editor/en/ConstructStringFromBinaryTree.java)|Easy|10-16-2020|
 |536|[$ Construct Binary Tree from String](https://leetcode.com/problems/construct-binary-tree-from-string/) [(Lintcode 880)](https://www.lintcode.com/problem/construct-binary-tree-from-string/description) | [Java](https://github.com/yiyiwii/leetcodeProb/blob/master/src/main/java/com/yiyiwii/leetcode/editor/en/ConstructBinaryTreeFromString.java)|Medium|10-17-2020|
 |652|[Find Duplicate Subtrees](https://leetcode.com/problems/find-duplicate-subtrees/)|[Java](https://github.com/yiyiwii/leetcodeProb/blob/master/src/main/java/com/yiyiwii/leetcode/editor/en/FindDuplicateSubtrees.java) | Medium | 10-18-2020|
-
-
+|701|[Insert into a Binary Search Tree](https://leetcode.com/problems/insert-into-a-binary-search-tree/)|[Java](https://github.com/yiyiwii/leetcodeProb/blob/master/src/main/java/com/yiyiwii/leetcode/editor/en/InsertIntoABinarySearchTree.java)|Medium|10-19-2020|
+|235|[Lowest Common Ancestor in Binary Search Tree](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/)|[Java](https://github.com/yiyiwii/leetcodeProb/blob/master/src/main/java/com/yiyiwii/leetcode/editor/en/LowestCommonAncestorOfABinarySearchTree.java)|Easy|10-20-2020|
+|236|[Lowest Common Ancestor in Binary Tree](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/)|[Java](https://github.com/yiyiwii/leetcodeProb/blob/master/src/main/java/com/yiyiwii/leetcode/editor/en/LowestCommonAncestorOfABinaryTree.java)|Medium|10-20-2020|
+   
 ### Microsoft Online Assessment Questions ([Link](https://leetcode.com/discuss/interview-question/398023/Microsoft-Online-Assessment-Questions))
 
 | Title | Solution | Date |
---
+++ src/main/java/com/yiyiwii/leetcode/editor/en/InsertIntoABinarySearchTree.java
@@ -0,0 +1,103 @@
+//You are given the root node of a binary search tree (BST) and a value to inser
+//t into the tree. Return the root node of the BST after the insertion. It is guar
+//anteed that the new value does not exist in the original BST. 
+//
+// Notice that there may exist multiple valid ways for the insertion, as long as
+// the tree remains a BST after insertion. You can return any of them. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [4,2,7,1,3], val = 5
+//Output: [4,2,7,1,3,5]
+//Explanation: Another accepted tree is:
+//
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = [40,20,60,10,30,50,70], val = 25
+//Output: [40,20,60,10,30,50,70,null,null,25]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+//Output: [4,2,7,1,3,5]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree will be in the range [0, 104]. 
+// -108 <= Node.val <= 108 
+// All the values Node.val are unique. 
+// -108 <= val <= 108 
+// It's guaranteed that val does not exist in the original BST. 
+// 
+// Related Topics Tree 
+// 👍 1213 👎 88
+
+
+package com.yiyiwii.leetcode.editor.en;
+public class InsertIntoABinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new InsertIntoABinarySearchTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * */
+ public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+class Solution {
+// Method 1:
+//    public TreeNode insertIntoBST(TreeNode root, int val) {
+//        if (root == null) {
+//            return new TreeNode(val);
+//        }
+//        if (val >= root.val) {
+//            root.right = insertIntoBST(root.right, val);
+//        } else {
+//            root.left = insertIntoBST(root.left, val);
+//        }
+//        return root;
+//    }
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        TreeNode curr = root;
+        TreeNode pre = null;
+        while (curr != null) {
+            pre = curr;
+            if (val > curr.val) {
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+        if (val > pre.val) {
+            pre.right = new TreeNode(val);
+        } else {
+            pre.left = new TreeNode(val);
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
