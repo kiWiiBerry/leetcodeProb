fd883394b033ef947b9f7aba8c0a67612e3077af
yiyiwii <donghua.cai@outlook.com>
2020-12-23 23:02:02 -0500

    update

---
+++ src/main/java/com/yiyiwii/leetcode/editor/en/ShortestPalindrome.java
@@ -0,0 +1,37 @@
+//Given a string s, you can convert it to a palindrome by adding characters in f
+//ront of it. Find and return the shortest palindrome you can find by performing t
+//his transformation. 
+//
+// 
+// Example 1: 
+// Input: s = "aacecaaa"
+//Output: "aaacecaaa"
+// Example 2: 
+// Input: s = "abcd"
+//Output: "dcbabcd"
+// 
+// 
+// Constraints: 
+//
+// 
+// 0 <= s.length <= 5 * 104 
+// s consists of lowercase English letters only. 
+// 
+// Related Topics String 
+// 👍 1428 👎 142
+
+
+package com.yiyiwii.leetcode.editor.en;
+public class ShortestPalindrome {
+    public static void main(String[] args) {
+        Solution solution = new ShortestPalindrome().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String shortestPalindrome(String s) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
