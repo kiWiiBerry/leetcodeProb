4125c1cb6f0712133b3d4c8e3674c29672be7c0a
yiyiwii <donghua.cai@outlook.com>
2020-12-26 10:05:11 -0500

    update

--- src/main/java/com/yiyiwii/leetcode/editor/en/SlidingWindowMaximum.java
+++ src/main/java/com/yiyiwii/leetcode/editor/en/SlidingWindowMaximum.java
@@ -79,11 +79,11 @@ class Solution {
         Deque<Integer> queue = new LinkedList<>();
         int[] res = new int[nums.length - k + 1];
         for (int i = 0; i < nums.length; i++) {
-            if (!queue.isEmpty() && queue.peekFirst() == (i - k)) {
-                queue.pollFirst();
+            if (!queue.isEmpty() && queue.peekFirst() < (i - k + 1)) {
+                queue.pollFirst();      // if this item is out of the window, move it
             }
             while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
-                queue.pollLast();
+                queue.pollLast();       // if items in queue are smaller than the new one, remove than from queue
             }
             queue.offerLast(i);
             if (i >= k - 1) {
