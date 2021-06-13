e23227e23ee5c7bd5a1d5083e3bcd1654ecf6af2
yiyiwii <donghua.cai@outlook.com>
2021-06-12 22:36:05 -0400

    update

--- src/main/java/com/yiyiwii/leetcode/editor/en/AmzStorageOptimization.java
+++ src/main/java/com/yiyiwii/leetcode/editor/en/AmzStorageOptimization.java
@@ -9,23 +9,22 @@ public class AmzStorageOptimization {
     }
     class Solution {
         public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-            int maxH = 0;
-            int maxV = 0;
-
             Arrays.sort(horizontalCuts);
             Arrays.sort(verticalCuts);
+            long maxH = horizontalCuts[0];
+            long maxV = verticalCuts[0];
 
             for (int i = 0; i < horizontalCuts.length; i++) {
-                maxH = Math.max(maxH, i == 0 ? horizontalCuts[i] : horizontalCuts[i] - horizontalCuts[i - 1]);
+                maxH = Math.max(maxH, horizontalCuts[i] - horizontalCuts[i - 1]);
             }
             maxH = Math.max(maxH, h - horizontalCuts[horizontalCuts.length - 1]);
 
             for (int i = 0; i < verticalCuts.length; i++) {
-                maxV = Math.max(maxV, i == 0 ? verticalCuts[i] : verticalCuts[i] - verticalCuts[i - 1]);
+                maxV = Math.max(maxV, verticalCuts[i] - verticalCuts[i - 1]);
             }
-            maxV = Math.max(maxV, h - verticalCuts[verticalCuts.length - 1]);
+            maxV = Math.max(maxV, w - verticalCuts[verticalCuts.length - 1]);
 
-            return (int)(maxH % (1e9 + 7) * maxV % (1e9 + 7));
+            return (int)((maxH * maxV) % 1_000_000_007);
         }
 
     }
--- src/main/java/com/yiyiwii/leetcode/editor/en/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.java
+++ src/main/java/com/yiyiwii/leetcode/editor/en/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.java
@@ -57,6 +57,9 @@
 
 
 package com.yiyiwii.leetcode.editor.en;
+
+import java.util.Arrays;
+
 public class MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts {
     public static void main(String[] args) {
         Solution solution = new MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts().new Solution();
@@ -64,7 +67,22 @@ public class MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-        
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        long maxH = horizontalCuts[0];
+        long maxV = verticalCuts[0];
+
+        for (int i = 1; i < horizontalCuts.length; i++) {
+            maxH = Math.max(maxH, horizontalCuts[i] - horizontalCuts[i - 1]);
+        }
+        maxH = Math.max(maxH, h - horizontalCuts[horizontalCuts.length - 1]);
+
+        for (int i = 1; i < verticalCuts.length; i++) {
+            maxV = Math.max(maxV, verticalCuts[i] - verticalCuts[i - 1]);
+        }
+        maxV = Math.max(maxV, w - verticalCuts[verticalCuts.length - 1]);
+
+        return (int)((maxH * maxV) % (1_000_000_007));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
